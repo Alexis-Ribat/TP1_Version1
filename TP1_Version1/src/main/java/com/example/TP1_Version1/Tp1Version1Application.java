80da44cbadb75b580556ecf6f00b19a7c67bb317
alrib <alrib@LAPTOP-CBOLI6AJ>
2019-11-19 15:05:27 +0100

    t

---
+++ TP1_Version1/src/main/java/com/example/TP1_Version1/Tp1Version1Application.java
@@ -0,0 +1,13 @@
+package com.example.TP1_Version1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1Version1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1Version1Application.class, args);
+	}
+
+}
---
+++ TP1_Version1/src/test/java/com/example/TP1_Version1/Tp1Version1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TP1_Version1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1Version1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
